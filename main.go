52b54fc139cfbc8ec90047f17f8cdd293b0b1fcb
Vilsol <vilsol2000@gmail.com>
2017-06-15 18:17:59 +0300

    Wrap encoding

--- main.go
+++ main.go
@@ -13,35 +13,66 @@ import (
 
 	"encoding/json"
 
+	"encoding/xml"
+
 	"github.com/julienschmidt/httprouter"
 )
 
 type ErrorResponse struct {
-	Code    int    `json:"code,omitempty"`
-	Message string `json:"message,omitempty"`
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Status  int    `json:"-"`
 }
 
 type GenericResponse struct {
-	Success bool           `json:"success,omitempty"`
+	Success bool           `json:"success"`
 	Error   *ErrorResponse `json:"error,omitempty"`
 	Data    *interface{}   `json:"data,omitempty"`
 }
 
+type GenericHandle func(*http.Request, httprouter.Params) GenericResponse
+
 type DataHandle func(*http.Request, httprouter.Params) (interface{}, *ErrorResponse)
 
-func DataHandler(handle DataHandle) httprouter.Handle {
+func ProcessResponse(handle GenericHandle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		response := handle(r, p)
+
+		if response.Error != nil {
+			w.WriteHeader(response.Error.Status)
+		}
+
+		format := p.ByName("format")
+
+		switch format {
+		case "json":
+		default:
+			w.Header().Set("Content-Type", "application/json")
+
+			encoder := json.NewEncoder(w)
+			encoder.SetIndent("", "    ")
+			encoder.Encode(response)
+			break
+		case "xml":
+			w.Header().Set("Content-Type", "application/xml")
+
+			encoder := xml.NewEncoder(w)
+			encoder.Indent("", "    ")
+			encoder.Encode(response)
+			break
+		}
+	}
+}
+
+func DataHandler(handle DataHandle) GenericHandle {
+	return func(r *http.Request, p httprouter.Params) GenericResponse {
 		data, err := handle(r, p)
 
-		response := GenericResponse{
+		return GenericResponse{
 			Success: err == nil,
 			Error:   err,
 			Data:    &data,
 		}
-
-		encoder := json.NewEncoder(w)
-		encoder.SetIndent("", "    ")
-		encoder.Encode(response)
 	}
 }
 
@@ -52,7 +83,7 @@ func main() {
 		fmt.Fprint(w, "Hello")
 	})
 
-	router.GET("/blueprint/search", DataHandler(Search))
+	router.GET("/blueprint/search", ProcessResponse(DataHandler(Search)))
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
