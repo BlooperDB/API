1e019435c41857c87aba91599682f6f43e047a5b
Vilsol <vilsol2000@gmail.com>
2017-06-15 17:47:58 +0300

    Initial API structure

--- main.go
+++ main.go
@@ -1,3 +1,9 @@
+// @APIVersion 1.0.0
+// @APITitle Blooper
+// @APIDescription Factorio blueprint database
+// @License MIT
+// @LicenseUrl https://opensource.org/licenses/MIT
+
 package main
 
 import (
@@ -5,14 +11,48 @@ import (
 	"log"
 	"net/http"
 
+	"encoding/json"
+
 	"github.com/julienschmidt/httprouter"
 )
 
+type ErrorResponse struct {
+	Code    int    `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+type GenericResponse struct {
+	Success bool           `json:"success,omitempty"`
+	Error   *ErrorResponse `json:"error,omitempty"`
+	Data    *interface{}   `json:"data,omitempty"`
+}
+
+type DataHandle func(*http.Request, httprouter.Params) (interface{}, *ErrorResponse)
+
+func DataHandler(handle DataHandle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		data, err := handle(r, p)
+
+		response := GenericResponse{
+			Success: err == nil,
+			Error:   err,
+			Data:    &data,
+		}
+
+		encoder := json.NewEncoder(w)
+		encoder.SetIndent("", "    ")
+		encoder.Encode(response)
+	}
+}
+
 func main() {
 	router := httprouter.New()
+
 	router.GET("/", func(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 		fmt.Fprint(w, "Hello")
 	})
+
+	router.GET("/blueprint/search", DataHandler(Search))
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
