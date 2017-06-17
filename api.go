79fb6564409691e835a3baa281b68f97d1ff9cdc
Vilsol <vilsol2000@gmail.com>
2017-06-17 19:11:21 +0300

    Wait up to 60 seconds

--- api.go
+++ api.go
@@ -7,9 +7,12 @@ import (
 
 	"log"
 
+	"time"
+
 	"github.com/BlooperDB/API/api"
 	"github.com/BlooperDB/API/blueprint"
 	"github.com/BlooperDB/API/db"
+	"github.com/BlooperDB/API/utils"
 	"github.com/gocql/gocql"
 	"github.com/gorilla/mux"
 )
@@ -25,17 +28,36 @@ func Initialize() {
 	blueprint.RegisterBlueprintRoutes(api.RouteHandler(router, "/v1"))
 
 	cluster := gocql.NewCluster("scylladb")
-	session, _ := cluster.CreateSession()
-	_, err := session.KeyspaceMetadata("blooper")
-	if err != nil {
-		session.Query("CREATE KEYSPACE blooper WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};").Exec()
+
+	for i := 0; i < 60; i++ {
+		stop := true
+
+		utils.Block{
+			Try: func() {
+				session, _ := cluster.CreateSession()
+				_, err := session.KeyspaceMetadata("blooper")
+				if err != nil {
+					session.Query("CREATE KEYSPACE blooper WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};").Exec()
+				}
+				session.Close()
+			},
+			Catch: func(e utils.Exception) {
+				stop = false
+				time.Sleep(1 * time.Second)
+			},
+			Finally: func() {
+			},
+		}.Do()
+
+		if stop {
+			break
+		}
 	}
-	session.Close()
 
 	cluster = gocql.NewCluster("scylladb")
 	cluster.Keyspace = "blooper"
 	cluster.Consistency = gocql.Quorum
-	session, _ = cluster.CreateSession()
+	session, _ := cluster.CreateSession()
 	defer session.Close()
 
 	db.Initialize(session)
